6629a8de5fd30280b561579ed4431d311674f07e
GK241196 <Gk52707@gmai.com>
2025-01-22 19:30:21 +0530

    New commit

---
+++ src/basepage/Test01.java
@@ -0,0 +1,12 @@
+package basepage;
+
+public class Test01 {
+	
+		public static void main(String args[]) {
+		 String input = "Ganesh kumar";
+		 System.out.println(input);
+		 String input1 = "Ganesh kumar";
+		 System.out.println(input1);
+		 //code change done
+}
+}
